2cc51d312ffdcaa71d62b99d5d4a659fe3b5edea
Simon Ashley <simon@rooksbridge.co.uk>
2024-09-11 15:06:27 +0100

    New device detection
    - Refactored the device type detection.
    - Added debug mode
    - Disabled sentry

--- main.py
+++ main.py
@@ -46,19 +46,23 @@ MQTT_ENV = os.environ.get("MQTT_ENV")
 BETTERSTACK_TOKEN = os.environ.get("BETTERSTACK_TOKEN")
 SENTRY_DSN=os.environ.get("SENTRY_DSN")
 BETTERSTACK_HEARTBEAT_URL=os.environ.get("BETTERSTACK_HEARTBEAT_URL")
+DEBUG_MODE = os.environ.get("DEBUG_MODE")
 
-# Setup Sentry
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-    release="feedalert_mqtt_to_postgresmyapp@1.0.0",
-)
+print(f"DEBUG_MODE: {DEBUG_MODE}")
+
+# if DEBUG_MODE != "True":
+#     # Setup Sentry
+#     sentry_sdk.init(
+#         dsn=SENTRY_DSN,
+#         # Set traces_sample_rate to 1.0 to capture 100%
+#         # of transactions for tracing.
+#         traces_sample_rate=1.0,
+#         # Set profiles_sample_rate to 1.0 to profile 100%
+#         # of sampled transactions.
+#         # We recommend adjusting this value in production.
+#         profiles_sample_rate=1.0,
+#         release="feedalert_mqtt_to_postgresmyapp@1.0.0",
+#     )
 
 # Create a CRC-32 checksum object
 crc32 = crcmod.predefined.Crc('crc-32')
@@ -67,14 +71,35 @@ crc32 = crcmod.predefined.Crc('crc-32')
 # The check is done only when a MQTT message arrives. Hence, this is to verify MQTT messages are flowing.
 last_heartbeat_time = time.time()
 
+if DEBUG_MODE != "True":
+    handler = LogtailHandler(source_token=BETTERSTACK_TOKEN)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.handlers = []
+    logger.addHandler(handler)
+else:
+    # Create a logger
+    logger = logging.getLogger('screen_logger')
 
-handler = LogtailHandler(source_token=BETTERSTACK_TOKEN)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.handlers = []
-logger.addHandler(handler)
+    # Set the minimum logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    logger.setLevel(logging.DEBUG)
 
-logger.info("Starting up...")
+    # Create a StreamHandler to log to the console (screen)
+    console_handler = logging.StreamHandler()
+
+    # Set the logging level for the handler
+    console_handler.setLevel(logging.DEBUG)
+
+    # Create a formatter for the log messages
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Attach the formatter to the handler
+    console_handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(console_handler)
+
+    logger.info("Starting up...")
 
 # Create a queue for messages that need to be written to the database
 write_queue = queue.Queue()
@@ -144,8 +169,6 @@ def on_disconnect(client, userdata, rc):
 def on_message(client, userdata, msg):
     try:
         
-        
-        
         # Create CRC from message and topic
         my_date = datetime.datetime.now() 
         
@@ -157,8 +180,6 @@ def on_message(client, userdata, msg):
         crc = crc + t
         # Add message and CRC to write queue if CRC is unique
         write_queue.put((my_date, imei, msg.topic, payload, crc))
-        
-       
         
     except Exception as e:
         logger.error("Error handling MQTT message: %s", str(e))
@@ -177,11 +198,15 @@ def write_to_database():
             cur = conn.cursor()
             while True:
                 
-                check_and_send_heartbeat()
+                if DEBUG_MODE != "True":
+                    check_and_send_heartbeat()
 
                 try:
                     # Get the next message from the write queue
                     message = write_queue.get(block=False)
+                    if DEBUG_MODE == "True":
+                        logger.info("Processing message: %s", str(message))
+
                 except queue.Empty:
                     break
                 try:
@@ -191,46 +216,71 @@ def write_to_database():
                     # Slice the string from the character after the last '/'
                     topic = message[2][last_slash_index + 1:]
                     imei = message[1]
+                    
+                    if DEBUG_MODE != "True":
+                        cur.execute("INSERT INTO mqtt (timestamp, imei, message, payload, crc, env, topic) VALUES (%s, %s, %s, %s, %s, %s, %s)", (*message,env,topic))
+                   
+                    
+                    device_type = ""
 
-                    if any(s in topic for s in ["swc","swd","mqtt_"]):
+                    cur.execute("SELECT swd_imei FROM things WHERE swd_imei = %s", (message[1],))
+                    # Fetch the result
+                    result = cur.fetchone()
+                    # Check if a row was returned and if the IMEI matches
+                    if result and result[0] == message[1]:
                         device_type = "old"
                     else:
                         device_type = "swx"
-                    
-                    cur.execute("INSERT INTO mqtt (timestamp, imei, message, payload, crc, env, topic) VALUES (%s, %s, %s, %s, %s, %s, %s)", (*message,env,topic))
-                    
-                    if device_type == "old":
-                        cur.execute("SELECT swd_imei FROM things WHERE swd_imei = %s", (message[1],))
-                    else:
+
+                    if device_type == "":
                         cur.execute("SELECT imei FROM things WHERE imei = %s", (message[1],))
+                        # Fetch the result
+                        result = cur.fetchone()
+                        # Check if a row was returned and if the IMEI matches
+                        if result and result[0] == message[1]:
+                            device_type = "old"
+                        else:
+                            device_type = "swx"
                     
-                    cur.fetchone()
                     
-                    if cur.rowcount > 0:
+                    if device_type != "":
+
                         if device_type == "old":
                             q = "UPDATE things SET " + topic + "='"+ message[3] +"', lastupdated=NOW() WHERE swd_imei = '"+ message[1] +"'"
-                        else:
+                        elif device_type == "swx":
                             q = "UPDATE things SET " + topic + "='"+ message[3] +"', lastupdated=NOW() WHERE imei = '"+ message[1] +"'"
-                        cur.execute(q)
+                        
+                            
+                        if device_type != "" and DEBUG_MODE != "True":
+                            cur.execute(q)
+                        else:
+                            logger.info(q)
 
                     else:
                         strings_to_check = ["connect", "connection", "disconnect", "location", "mqttstats"]
-                        if any(s in topic for s in strings_to_check):
-                            xxx=1
-                        else:
+                        if not any(s in topic for s in strings_to_check):
                             if device_type == "old":
                                 q = "INSERT INTO things (swd_imei, " + topic + "," + "lastupdated) VALUES ('" + message[1] +"', '" + message[3] + "', NOW())"
-                            else:
+                            elif device_type == "swx":
                                 q = "INSERT INTO things (imei, " + topic + "," + "lastupdated) VALUES ('" + message[1] +"', '" + message[3] + "', NOW())"
-                            cur.execute(q)
+                            
+                            if DEBUG_MODE != "True":
+                                cur.execute(q)
+                            else:
+                                logger.info(q)
 
                 except errors.UndefinedColumn as e:
                         conn.rollback()
                         logger.warning("Column not present: %s", str(e))
                         logger.info("Creating column: ", topic)
                         sql="ALTER TABLE things ADD COLUMN " + topic + " TEXT;"
-                        cur.execute(sql)
-                        conn.commit()
+                        if DEBUG_MODE != "True":
+                            cur.execute(sql)
+                            conn.commit()
+                        else:
+                            logger.info(sql)
+                        
+                        
                         
                         
                 
